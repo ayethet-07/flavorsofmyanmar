df0135695372e0b038c9deec58c070c912f00594
Aye Thet Thet Khaing <ayethet.07@gmail.com>
2026-02-17 14:26:45 +0100

    Add basic routes for recipes

--- routes/recipes.js
+++ routes/recipes.js
@@ -1,1 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.json({ message: 'Get all recipes - coming soon' });
+});
+
+router.post('/', (req, res) => {
+  res.json({ message: 'Create recipe - coming soon' });
+});
+
+module.exports = router;
 
