90faf1c6eef9f06a16475084dc1a32d92ba2fc99
Aye Thet Thet Khaing <ayethet.07@gmail.com>
2026-02-17 15:09:52 +0100

    Update ebook.js

--- routes/ebook.js
+++ routes/ebook.js
@@ -1,1 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.json({ message: 'Get all ebooks - coming soon' });
+});
+
+module.exports = router;
 
