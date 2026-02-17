c9af26ae964285863037a258e13e816d3d0c7e01
Aye Thet Thet Khaing <ayethet.07@gmail.com>
2026-02-17 14:28:16 +0100

    Update workshops.js

--- routes/workshops.js
+++ routes/workshops.js
@@ -1,1 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.json({ message: 'Get all workshops - coming soon' });
+});
+
+module.exports = router;
 
